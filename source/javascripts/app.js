613fe99e95f4035c93382368fdbac321281827db
Steven Garcia <stevendgarcia@gmail.de>
2012-04-11 18:20:09 +0200

    Menu tweaks

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -4,7 +4,7 @@ $(function() {
 	$('#advanced_search').hide();
 	$('#toggle_advanced_search').click(function() {
 	  var self = $(this)
-	  $('#advanced_search').toggle();
+	  $('#advanced_search').toggle('slide');
 	  self.toggleClass('open');
 	  if ( self.hasClass('open') ) { self.html('Fewer options'); }
 	  else { self.html('Search more options'); }
