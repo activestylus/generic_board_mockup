98da98c84fddde87dd7ed253aab3744a56358515
Steven Garcia <stevendgarcia@gmail.de>
2012-04-13 16:55:17 +0200

    Homepage style and JS enhancements

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,14 +1,39 @@
 $(function() {
 
-	$('.scroll-pane').jScrollPane({ showArrows: true });
-	$('#advanced_search').hide();
-	$('#toggle_advanced_search').click(function() {
-	  var self = $(this)
-	  $('#advanced_search').toggle('slide');
-	  self.toggleClass('open');
-	  if ( self.hasClass('open') ) { self.html('Fewer options'); }
-	  else { self.html('Search more options'); }
-	  console.log($(this).html());
-	});
+  //$('.scroll-pane').jScrollPane({ showArrows: true });
+
+  var api = $('.scroll-pane').jScrollPane({ showArrows: true }).data('jsp');
+  $('.jspArrowDown').bind('mouseover', function() {
+    var scrollInterval,
+    link = $(this),
+    doScroll = function() { api.scrollByY(5) };
+    link.bind('mouseout.demo', function() {
+      clearInterval(scrollInterval); link.unbind('mouseout.demo');
+    });
+    doScroll();
+    scrollInterval = setInterval(doScroll, 50);
+  });
+  $('.jspArrowUp').bind('mouseover', function() {
+    var scrollInterval,
+    link = $(this),
+    doScroll = function() { api.scrollByY(-5) };
+    link.bind('mouseout.demo', function() {
+      clearInterval(scrollInterval); link.unbind('mouseout.demo');
+    });
+    doScroll();
+    scrollInterval = setInterval(doScroll, 50);
+  });
+
+
+  $('#advanced_search').hide();
+  $('#toggle_advanced_search').click(function() {
+    var self = $(this);
+    var div = $('#advanced_search');
+    if (div.is(":hidden")) { div.slideDown(); } else { div.slideUp(); }
+    self.toggleClass('open');
+    if ( self.hasClass('open') ) { self.html('Fewer options'); }
+    else { self.html('Search more options'); }
+    console.log($(this).html());
+  });
 
 });
